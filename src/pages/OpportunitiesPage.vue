306e2326ace858589955b1ce7ab1ad179b436af8
Wendy Afrim <wendy.afrim2@gmail.com>
2023-08-21 08:01:31 +0200

    feat(dashboard): Reintegrate Opportunities view with table search

--- src/pages/OpportunitiesPage.vue
+++ src/pages/OpportunitiesPage.vue
@@ -4,7 +4,26 @@
         <q-table title="Consultants" :rows="rows" :columns="columns" row-key="name" selection="multiple"
             v-model:selected="selected" :filter="option" :filter-method="filterData">
             <template v-slot:top>
-                <slot name="body"></slot>
+                <q-tr>
+                    <q-td>
+                    </q-td>
+                    <q-td>
+                        <q-input outlined v-model="option.email" label="Filtre Email" dense></q-input>
+                    </q-td>
+                    <q-td>
+                        <q-input outlined v-model="option.position" label="Filtre Poste" dense></q-input>
+                    </q-td>
+                    <q-td>
+                        <q-input outlined v-model="option.skills" label="Filtre Compétences" dense></q-input>
+                    </q-td>
+                    <q-td>
+                        <q-input outlined v-model="option.end_at" label="Filtre Date de début" dense></q-input>
+                    </q-td>
+                    <q-td>
+                        <q-input outlined v-model="option.state" label="Filtre Etat" dense></q-input>
+                    </q-td>
+                </q-tr>
+                <!-- <slot name="body"></slot> -->
             </template>
         </q-table>
     </div>
@@ -12,22 +31,58 @@
 <script setup lang="ts">
 import { reactive, ref } from 'vue';
 
-const props = defineProps({
-    title: {
-        type: String
-    },
-    columns: {
-        type: Array
-    },
-    rows: {
-        type: Array
-    },
-    option: {
-        type: Array
-    }
-})
 
 const selected = ref([]);
+
+const columns = [
+    {
+        name: 'firstname',
+        required: true,
+        label: 'Prénom',
+        align: 'center',
+        field: 'firstname',
+        sortable: true
+    },
+    { name: 'lastname', align: 'center', label: 'Nom', field: 'lastname', sortable: true },
+    { name: 'email', align: 'center', label: 'Email', field: 'email', sortable: true },
+    { name: 'position', align: 'center', label: 'Poste', field: 'position', sortable: true },
+    { name: 'begin_at', align: 'center', label: 'Date de début', field: 'begin_at', sortable: true },
+    { name: 'skills', label: 'Compétences', field: 'skills' },
+    { name: 'state', label: 'Statut', field: 'state' },
+    { name: 'actions', label: 'Actions', field: 'actions' },
+]
+
+
+const rows = [
+    {
+        lastname: 'Dupont',
+        firstname: "Jean",
+        email: "jean@carbon.com",
+        position: "Developpeur React",
+        begin_at: "06/06/2025",
+        skills: "JS, Vue",
+        state: 'Libre',
+        actions: '1%'
+    },
+    {
+        lastname: 'Ferreira',
+        firstname: "Kevin",
+        email: "kevin@carbon.com",
+        position: "Developpeur Python",
+        begin_at: "06/06/2025",
+        skills: "Python, Django",
+        state: 'Occupé',
+        actions: '1%'
+    },
+]
+
+const option = reactive({
+    email: '',
+    position: '',
+    skills: '',
+    end_at: '',
+    state: '',
+})
 
 
 
