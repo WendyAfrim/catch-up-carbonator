ffd119e621d6838c399a74bd730d3803db54a234
Wendy Afrim <wendy.afrim2@gmail.com>
2023-09-12 16:40:23 +0200

    quick fix

--- src/pages/Dashboard/RhDashboard.vue
+++ src/pages/Dashboard/RhDashboard.vue
@@ -132,34 +132,6 @@
                 </q-tr>
               </div>
             </template>
-            <template v-slot:header="props">
-              <q-tr :props="props">
-                <q-th auto-width/>
-                <q-th v-for="col in props.cols" :key="col.name" :props="props">
-                  {{ col.label }}
-                </q-th>
-              </q-tr>
-            </template>
-
-            <template v-slot:body="props">
-              <q-tr :props="props">
-                <q-td auto-width>
-                  <q-btn size="sm" color="green" round dense @click="props.expand = !props.expand"
-                         :icon="props.expand ? 'remove' : 'add'"/>
-                </q-td>
-                <q-td v-for="col in props.cols" :key="col.name" :props="props">
-                  {{ col.value }}
-                </q-td>
-              </q-tr>
-              <q-tr v-show="props.expand" :props="props">
-                <q-td colspan="100%">
-                  <div class="text-left">
-                    Description du profil :
-                    <br>
-                  </div>
-                </q-td>
-              </q-tr>
-            </template>
           </q-table>
         </div>
       </div>
